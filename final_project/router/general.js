17cd074297497d8c62e670522a1775bcbfb1de69
Your Name <user@example.com>
2025-05-09 18:29:22 -0400

    Implement review endpoint to get book reviews by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -85,8 +85,19 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Get the ISBN from request parameters
+    const isbn = req.params.isbn;
+    
+    // Find the book with the matching ISBN
+    const book = books[isbn];
+    
+    if (book) {
+        // Return the reviews if book is found
+        return res.status(200).send(JSON.stringify(book.reviews, null, 4));
+    } else {
+        // Return error if book not found
+        return res.status(404).json({ message: "Book not found" });
+    }
 });
 
 module.exports.general = public_users;
