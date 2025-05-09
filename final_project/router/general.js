3806e9150909c348d0c73c9fcd13c338ae195bc0
Your Name <user@example.com>
2025-05-09 17:24:19 -0400

    Implemented GET endpoint for books list with formatted JSON output

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,8 +12,8 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Return the complete list of books with formatted JSON output
+  return res.status(300).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
